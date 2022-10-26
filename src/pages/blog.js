a989b5c049a5c622d9d3964a45795a70abe0d6ac
cpacifico <cpacifico@sogei.it>
2022-10-26 11:30:51 +0200

    part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,35 +1,39 @@
-import * as React from 'react'
-import Layout from '../components/layout'
-import Seo from '../components/seo'
-import { graphql } from 'gatsby'
-
+import * as React from "react";
+import Layout from "../components/layout";
+import Seo from "../components/seo";
+import { graphql } from "gatsby";
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
       {
-        data.allFile.nodes.map(node => (
-          <li key={node.name}>
-            {node.name}
-          </li>
+        data.allMdx.nodes.map((node) => (
+          <article key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>Posted: {node.frontmatter.date}</p>
+            <p>{node.excerpt}</p>
+          </article>
         ))
       }
-      </ul>
     </Layout>
-  )
-}
+  );
+};
 
 export const query = graphql`
   query {
-    allFile {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
       nodes {
-        name
+        frontmatter {
+          title
+          date(formatString: "MMMM DD, YYYY")
+        }
+        id
+        excerpt
       }
     }
   }
 `
 
-export const Head = () => <Seo title="My Blog Posts" />
+export const Head = () => <Seo title="My Blog Posts" />;
 
-export default BlogPage
+export default BlogPage;
